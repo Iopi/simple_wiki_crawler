e2c125976486f24372d78ac4c2c7ab9ead59becd
Iopi <33093127+Iopi@users.noreply.github.com>
2022-08-04 13:02:13 +0200

    Repaired version

--- wiki_craw.py
+++ wiki_craw.py
@@ -1,61 +1,111 @@
+import signal
+import time
+import readchar
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
+BASE_URL = 'https://cs.wikipedia.org/w/index.php?search='
+
+def output(text):
+    """ Sends resulting text to output """
+    print(text)
+
+def handler(signum, frame):
+    """ Ctrl+c handler """
+    res = input("Bylo stisknuto Ctrl-c. Prejes si ukoncit program? ano/ne ")
+    if res == 'ano':
+        exit(1)
+
+def crawling(memory, title):
+    """ Sends resulting text from crawling to output """
+    # obtaining resources
+    url = BASE_URL + title
+    try:
+        source = urlopen(url).read()
+    except:
+        output("Nelze cist url. Ukonceni programu.")
+        exit(1)
+    soup = BeautifulSoup(source,'lxml')
+
+    heading = soup.find_all('h1', attrs={'firstHeading'})[0].text
+
+    # if result is not found
+    if heading == "Výsledky hledání":
+        memory[title] = ["0"]
+        output("Článek s názvem '" + title + "' nebyl nalezen. Zadaný text se vyskytuje v článcích s tímto názvem:")
+        for similar_header in soup.find_all('div', attrs={'mw-search-result-heading'}):
+            memory[title].append(similar_header.text)
+            output(similar_header.text)
+            
+    # result is found
+    else:
+        paragraph = soup.find_all('p')[0].text
+        memory[title] = ["1"]
+        memory[title].append(paragraph)
+        output(paragraph)
+
+def get_memory_text(memory, title):
+    """ Sends resulting text from memory to output """
+    if memory[title][0] == "1":
+        output(memory[title][1])
+    else:
+        output("Článek s názvem '" + title + "' nebyl nalezen. Zadaný text se vyskytuje v článcích s tímto názvem:")
+        for similar_header in memory[title][1:]:
+            output(similar_header)
+
+
+def get_text(memory, title):
+    """ Sends resulting text from memory or crawling to output """
+    # if title already exists in memory
+    if title in memory:
+        get_memory_text(memory, title)
+        
+    else:
+        crawling(memory, title)
+        
 def main():
 
     continue_all = True
     memory = dict()
 
+    signal.signal(signal.SIGINT, handler)
+
     while(continue_all):
 
-        title = input("Nazev hledane stranky na wikipedii:")
-        print("\n")
+        title = ''
+        try:
+            title = input("Nazev hledane stranky na wikipedii:")
+        except RuntimeError:
+            pass
+        output("\n")
+        
+        if title:
+            title = title.lower()
+            get_text(memory, title)
 
-        # if title already exists in memory
-        if title in memory.keys():
-            if memory[title][0] == "1":
-                print(memory[title][1])
-            else:
-                print("Článek s názvem '" + title + "' nebyl nalezen. Zadaný text se vyskytuje v článcích s tímto názvem:")
-                for similar_header in memory[title][1:]:
-                    print(similar_header)
         else:
-            # obtaining resources
-            url = 'https://cs.wikipedia.org/w/index.php?search=' + title
-            source = urlopen(url).read()
-            soup = BeautifulSoup(source,'lxml')
-
-            heading = soup.find_all('h1', attrs={'firstHeading'})[0].text
-
-            # if result is not found
-            if heading == "Výsledky hledání":
-                memory[title] = []
-                memory[title].append("0")
-                print("Článek s názvem '" + title + "' nebyl nalezen. Zadaný text se vyskytuje v článcích s tímto názvem:")
-                for similar_header in soup.find_all('div', attrs={'mw-search-result-heading'}):
-                    memory[title].append(similar_header.text)
-                    print(similar_header.text)
-                    
-            # result is found
-            else:
-                paragraph = soup.find_all('p')[0].text
-                memory[title] = []
-                memory[title].append("1")
-                memory[title].append(paragraph)
-                print(paragraph)
+            output("Prazdny vstup")
 
         # ask for repeating
         continue_ending = True
         while(continue_ending):
-            input_continue = input("\nPrejes si pokracovat? (y/n):")
-            if input_continue == 'n':
+            input_continue = ''
+            try:
+                input_continue = input("\nPrejes si pokracovat? (ano/ne):")
+            except RuntimeError:
+                pass
+
+            input_continue = input_continue.lower()
+            if input_continue == 'ne':
                 continue_all = False
                 continue_ending = False
-                print("\nUkonceni programu.")
-            elif input_continue == 'y':
+                output("\nUkonceni programu.")
+
+            elif input_continue == 'ano':
                 continue_ending = False
             else:
-                print("\nSpatny vstup.")
+                output("\nSpatny vstup.")
 
 
 if __name__ == "__main__":
